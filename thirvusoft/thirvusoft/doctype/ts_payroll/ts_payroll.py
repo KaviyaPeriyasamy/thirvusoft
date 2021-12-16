379915a6b8e5e1c6b1a7af475385565cb3700a76
Shankarv19bcr <shankar@thirvusoft.in>
2021-12-16 15:03:04 +0530

    Report for salary calculation with one doctype

---
+++ thirvusoft/thirvusoft/doctype/ts_payroll/ts_payroll.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021, Thirvusoft and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class TSPayroll(Document):
+	def validate(self):
+		if(len(self.ts_phone) != 10 or int(self.ts_phone[0]) not in range(6,10)):
+			frappe.throw("Invalid Phone Number")
+		else:
+			self.ts_phone=self.ts_phone
+		
+		d={"Software Developer":8100,"Designer":8000,"Marketing":10000,"Tech lead":30000,"House Keeping":11000}
+		self.ts_basic=d[self.ts_role]
+		if(int(self.ts_from[-1:-2])==int(self.ts_to[-1:-2]) or int(self.ts_from[-4:-5])!=int(self.ts_to[-4:-5]) ):
+			frappe.throw("Enter valid date")
