cca8b5d9a119d4769ddee8804f496e18dc98fdf5
Shankarv19bcr <shankar@thirvusoft.in>
2021-12-17 12:16:39 +0530

    Dynamic Filter added

--- thirvusoft/thirvusoft/doctype/ts_payroll/ts_payroll.py
+++ thirvusoft/thirvusoft/doctype/ts_payroll/ts_payroll.py
@@ -13,5 +13,5 @@ class TSPayroll(Document):
 		
 		d={"Software Developer":8100,"Designer":8000,"Marketing":10000,"Tech lead":30000,"House Keeping":11000}
 		self.ts_basic=d[self.ts_role]
-		if(int(self.ts_from[-1:-2])==int(self.ts_to[-1:-2]) or int(self.ts_from[-4:-5])!=int(self.ts_to[-4:-5]) ):
-			frappe.throw("Enter valid date")
+		#if(int(self.ts_from[-1:-2])==int(self.ts_to[-1:-2]) or int(self.ts_from[-4:-5])!=int(self.ts_to[-4:-5]) ):
+			#frappe.throw("Enter valid date")
---
+++ thirvusoft/thirvusoft/report/salary_slip_script/salary_slip_script.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013, Thirvusoft and contributors
+# For license information, please see license.txt
+# Function name =>> get_data, get_column 
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
